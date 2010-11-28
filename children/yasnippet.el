4e78c8fdf3b67c51120a8f31eaab4b2776dff6e2
Michael Budde <mbudde@gmail.com>
2010-11-28 18:06:18 +0100

    Use ido prompt for choosing fields in yas-snippets

--- children/yasnippet.el
+++ children/yasnippet.el
@@ -8,3 +8,7 @@
         "~/.emacs.d/support/yasnippet/snippets"))
 (mapcar 'yas/load-directory yas/root-directory)
 (add-to-list 'hippie-expand-try-functions-list 'yas/hippie-try-expand) ;; Expand snippets with hippie expand
+
+(setq yas/prompt-functions
+      '(yas/ido-prompt yas/x-prompt yas/dropdown-prompt
+        yas/completing-prompt yas/no-prompt))
